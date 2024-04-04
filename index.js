a5380e07f5b5d8513d34fbdad065f44241a0ecc5
sunil kumar sahoo <sk7997717@gmail.com>
2024-04-04 10:43:41 +0530

    database connected sucessfully

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
+require('dotenv').config(); //needed to get the value from the .env file
 const express = require('express');
-
 const server = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+
+//for connecting with db
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect(process.env.DB_URL);
+    console.log("Db connected successfully");
+}
+
+// middilewire for the express project(server)
+
+server.use(cors());
+server.use(express.json()); //This line adds middleware to parse JSON bodies sent in the HTTP request
+
+
+
+
+server.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
 
 
 server.listen(process.env.PORT, () => {
