18413aab1707246966cea54a8381bed8d79c47ed
sunil kumar sahoo <sk7997717@gmail.com>
2024-04-10 13:54:20 +0530

    ...

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ server.set("views", path.resolve("./view"));
 const corsOptions = {
     origin: 'http://localhost:5173', // Specify the allowed origin
     methods:['GET','POST','DELETE','PUT'],
+    header:'Access-Control-Allow-Origin:*',
     Credentials:true,
 };
 
